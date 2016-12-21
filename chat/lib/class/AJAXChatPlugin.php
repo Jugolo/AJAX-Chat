d6cce3ffa2134e0dacf06e059ddd822f5aaf2e7c
Ronnie <rix17172@gmail.com>
2016-12-21 11:40:25 +0100

    Update AJAXChatPlugin.php

--- chat/lib/class/AJAXChatPlugin.php
+++ chat/lib/class/AJAXChatPlugin.php
@@ -1,1 +1,26 @@
 <?php
+class AJAXChatPlugin{
+  private $plugin = [];
+  
+  public function __construct($ajaxchat){
+    foreach($ajaxchat->getConfig("plugin") as $dir){
+      if(file_exists("./lib/plugin/".$dir."/plugin.php")){
+        $this->loadPlugin($dir);
+      }
+    }
+  }
+  
+  public function event($name, $arg){
+    foreach($this->plugin as $plugin){
+      if(!call_user_func_array([$plugin, "event"], $arg))
+        return false;
+    }
+    return true;
+  }
+  
+  private function loadPlugin($dir){
+    include "./lib/plugin/".$dir."/plugin.php";
+    if(class_exists($dir))
+      $this->plugin[] = new $dir();
+  }
+}
